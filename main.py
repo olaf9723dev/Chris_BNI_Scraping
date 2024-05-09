832fd06df5caa7bb43ab90b94c3598f103e2ddc7
olaf9723dev <olaf.9723.dev@gmail.com>
2024-05-09 06:08:18 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from playwright.sync_api import sync_playwright
+import requests
+from bs4 import BeautifulSoup
+
+class Extractor:
+    def __init__(self) -> None:
+        pass
+def main():
+    pass
